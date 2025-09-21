2b77a41cc65e62b8df1eb738564623b9b085179d
hey-anna <heyannawp@gmail.com>
2025-09-22 01:38:25 +0900

    chore : storybook 초기 설정 + 전역 CSS 연결 (온보딩 샘플 포함)
    - 프레임워크 @storybook/nextjs로 통일
    - preview.ts 타입 import 일치

---
+++ .storybook/main.ts
@@ -0,0 +1,30 @@
+// .storybook/main.ts
+export default {
+  stories: ["../src/**/*.stories.@(tsx|mdx)", "../src/stories/**/*.mdx"],
+  addons: ["@storybook/addon-essentials"],
+  framework: { name: "@storybook/nextjs", options: { appDirectory: true } },
+};
+
+// import type { StorybookConfig } from "@storybook/nextjs-vite";
+
+// const config: StorybookConfig = {
+//   "stories": [
+//     "../src/**/*.mdx",
+//     "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"
+//   ],
+//   "addons": [
+//     "@chromatic-com/storybook",
+//     "@storybook/addon-docs",
+//     "@storybook/addon-onboarding",
+//     "@storybook/addon-a11y",
+//     "@storybook/addon-vitest"
+//   ],
+//   "framework": {
+//     "name": "@storybook/nextjs-vite",
+//     "options": {}
+//   },
+//   "staticDirs": [
+//     "../public"
+//   ]
+// };
+// export default config;
