ce51e6170639a1de358602dbe51591db85c02ed2
Yoos99 <yoosh2520@naver.com>
2025-09-24 09:26:00 +0900

    style: #6 input 컴포넌트 스타일링

--- src/components/ui/example/input_03.tsx
+++ src/components/ui/example/input_03.tsx
@@ -8,26 +8,25 @@ const inputVariants = cva(
     "selection:bg-primary selection:text-primary-foreground",
     "dark:bg-input/30",
     "border-input bg-transparent",
-    "flex w-full min-w-0 rounded-md border",
+    "flex w-full min-w-0 rounded-xl border",
     "shadow-xs transition-[color,box-shadow,border-color]",
     "outline-none file:inline-flex file:h-7 file:border-0 file:bg-transparent file:text-sm file:font-medium",
     "disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50 md:text-sm",
-    " focus-visible:ring-transparent focus-visible:border-purple-500",
+    " focus-visible:border-purple-500 focus-visible:ring-2 focus-visible:ring-purple-500 focus-visible:ring-offset-0",
   ].join(" "),
   {
     variants: {
-      // Figma 기준: sm 40px, md 48px, lg 56px
       size: {
-        sm: "h-10 px-4 text-sm",
-        md: "h-12 px-4",
-        lg: "h-14 px-4 text-base",
+        sm: "h-10 px-4 ",
+        md: "h-12 px-4 ",
+        lg: "h-14 px-4 ",
       },
       state: {
         default: "",
         invalid: "border-red-500",
         success: "border-purple-500",
       },
-      // 아이콘 존재 시: padding = 16(우/좌) + 24(아이콘) + 10(gap) = 50px
+
       withStartIcon: { true: "pl-[50px]", false: "" },
       withEndIcon: { true: "pr-[50px]", false: "" },
     },
