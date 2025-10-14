fc021db344f612aaebcedc8b87f45d3874be1a31
callu9 <callu_9ine@naver.com>
2025-10-14 17:41:33 +0900

    chore: 프로필 조회 및 업데이트 mock data 추가

---
+++ src/mocks/my/mockProfile.ts
@@ -0,0 +1,10 @@
+export const mockProfile = {
+  companyName: "무한상사",
+  createdAt: "2025-09-19T06:39:00.806Z",
+  email: "callu_9ine@naver.com",
+  id: 1,
+  image:
+    "https://sprint-fe-project.s3.ap-northeast-2.amazonaws.com/together-dallaem/1760430046659_images.jpeg",
+  name: "이수정",
+  updatedAt: "2025-10-14T08:30:02.708Z",
+};
