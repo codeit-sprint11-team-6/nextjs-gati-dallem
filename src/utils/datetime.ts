ddf2ef71de61f97679adad12516a5a2f0ea6d9fb
callu9 <callu_9ine@naver.com>
2025-10-15 19:59:56 +0900

    fix: datetime 포맷터 유틸 함수 수정 (한국 시간 기준 표시 위함)

--- src/utils/datetime.ts
+++ src/utils/datetime.ts
@@ -1,25 +1,23 @@
 import { formatInTimeZone } from "date-fns-tz";
 
 /**
- * 날짜, 시간 포맷터
- * @param datetime ISO 문자열
+ * 날짜, 시간 포맷터 (한국 시간 기준)
+ * @param datetime ISO 문자열 혹은 Date 객체
  * @returns [date, time]
  */
-export const formatDateAndTime = (datetime: string) => {
-  const naive = datetime.replace(/Z$/, "");
+export const formatDateAndTime = (datetime: string | Date) => {
   const timeZone = "Asia/Seoul";
-  const date = formatInTimeZone(naive, timeZone, "MM월 dd일");
-  const time = formatInTimeZone(naive, timeZone, "HH:mm");
+  const date = formatInTimeZone(datetime, timeZone, "MM월 dd일");
+  const time = formatInTimeZone(datetime, timeZone, "HH:mm");
   return [date, time];
 };
 
 /**
- * 날짜 포맷터
- * @param datetime ISO 문자열
+ * 날짜 포맷터 (한국 시간 기준)
+ * @param datetime ISO 문자열 혹은 Date 객체
  * @returns yyyy.MM.dd
  */
-export const formatDate = (datetime: string) => {
-  const naive = datetime.replace(/Z$/, "");
+export const formatDate = (datetime: string | Date) => {
   const timeZone = "Asia/Seoul";
-  return formatInTimeZone(naive, timeZone, "yyyy.MM.dd");
+  return formatInTimeZone(datetime, timeZone, "yyyy.MM.dd");
 };
