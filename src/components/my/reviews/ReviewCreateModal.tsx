00cc014df6cc06763d70f3632605ad0b8793c25a
callu9 <callu_9ine@naver.com>
2025-10-15 12:18:36 +0900

    feat: 리뷰 작성 모달 UI 개발 및 API 연결

--- src/components/my/reviews/ReviewCreateModal.tsx
+++ src/components/my/reviews/ReviewCreateModal.tsx
@@ -2,18 +2,46 @@
 
 import { useOverlay } from "@/hooks/useOverlay";
 import Modal from "../../common/Modal";
+import ScoreInput from "./ScoreInput";
+import { useState } from "react";
+import { useCreateReview } from "@/apis/reviews/reviews.query";
 
-export default function ReviewCreateModal() {
+export default function ReviewCreateModal({ id }: { id: number }) {
+  const [score, setScore] = useState<number>(0);
+  const [comment, setComment] = useState<string>("");
+
+  const { mutate: createReviewMutate } = useCreateReview();
   const { close } = useOverlay();
 
   function handleBtnClick() {
-    // TODO: 리뷰 작성
-    close();
+    createReviewMutate({ gatheringId: id, score, comment }, { onSuccess: () => close() });
   }
   return (
     <Modal>
       <Modal.Header>리뷰 쓰기</Modal.Header>
-      <Modal.TwoButton onRightBtnClick={handleBtnClick} />
+      <div className="grid justify-stretch gap-12">
+        <div className="grid justify-stretch gap-2.5">
+          <label htmlFor="score" className="font-medium text-slate-800 md:text-lg">
+            만족스러운 경험이었나요?
+          </label>
+          <ScoreInput {...{ score, setScore }} />
+        </div>
+        <div className="grid justify-stretch gap-2.5">
+          <label htmlFor="comment" className="font-medium text-slate-800 md:text-lg">
+            경험에 대해 남겨주세요.
+          </label>
+          <div className="h-[200px] rounded-xl bg-gray-50 p-4 focus-within:ring-2 focus-within:ring-purple-500">
+            <textarea
+              id="comment"
+              className="h-full w-full resize-none overflow-y-auto border-none text-sm ring-0 ring-offset-0 placeholder:text-slate-500 md:text-base"
+              placeholder="남겨주신 리뷰는 프로그램 운영 및 다른 회원 분들께 큰 도움이 됩니다."
+              value={comment}
+              onChange={(e) => setComment(e.target.value)}
+            />
+          </div>
+        </div>
+      </div>
+      <Modal.TwoButton onRightBtnClick={handleBtnClick} rightBtnDisabled={!!!score || !!!comment} />
     </Modal>
   );
 }
