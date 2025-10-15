72ea6bb78bd03a5fc474d1168252440ee5a73811
callu9 <callu_9ine@naver.com>
2025-10-15 23:48:24 +0900

    fix: Storybook import 오류 수정 refactor: Storybook 공통 카드 컴포넌트 구조 스토리 분리

--- src/stories/common/Card.stories.tsx
+++ src/stories/common/Card.stories.tsx
@@ -1,8 +1,6 @@
 import { Card } from "@/components/common/Card";
-import ReservedCardItem from "@/components/my/bookings/ReservedCardItem";
-import CreatedCardItem from "@/components/my/hosted/CreatedCardItem";
-import UnreviewedCardItem from "@/components/my/reviews/UnreviewedCardItem";
 import { ChipInfo } from "@/components/ui/Chip";
+import { mockMyGathering } from "@/mocks/my/mockMyGathering";
 import type { Meta, StoryObj } from "@storybook/react";
 
 const meta = {
@@ -17,31 +15,17 @@ export default meta;
 
 type Story = StoryObj<typeof meta>;
 
-const mockData = {
-  teamId: "11-6",
-  id: 0,
-  type: "DALLAEMFIT",
-  name: "같이달램 프론트엔드 네트워킹",
-  dateTime: "2025-09-23T00:16:21.224Z",
-  registrationEnd: "2025-09-23T00:16:21.224Z",
-  location: "강남",
-  participantCount: 18,
-  capacity: 20,
-  image:
-    "https://t1.kakaocdn.net/thumb/C630x354.fwebp.q100/?fname=https%3A%2F%2Ft1.kakaocdn.net%2Fkakaocorp%2Fkakaocorp%2Fadmin%2Fnews%2F76058ec8019900001.jpeg",
-  createdBy: 0,
-  canceledAt: "2025-09-23T00:16:21.224Z",
-  joinedAt: "2025-09-23T00:16:21.224Z",
-  isCompleted: true,
-  isReviewed: true,
-};
-
+/**
+ * **Card 컴포넌트의 요소를 조합**하여 커스텀 사용할 수 있으며,
+ * 타 UI 컴포넌트와의 결합 또한 가능합니다.
+ * - Card 컴포넌트의 요소: Image, Detail, Tags, Title, GatheringDetail ...
+ */
 export const CustomCardExample: Story = {
   render: () => {
-    const { id, name } = mockData;
+    const { id, name, image } = mockMyGathering[1];
     return (
       <Card>
-        <Card.Image image={mockData.image} />
+        <Card.Image image={image} />
         <Card.Detail>
           <div className="flex h-full flex-col items-start justify-between gap-4">
             <Card.Title id={id}>
@@ -62,17 +46,4 @@ export const CustomCardExample: Story = {
       </Card>
     );
   },
-};
-
-/** 마이페이지 나의 모임 카드 컴포넌트 */
-export const CardReserved: Story = {
-  render: () => <ReservedCardItem {...mockData} />,
-};
-/** 마이페이지 나의 리뷰 - 작성 가능한 리뷰 카드 컴포넌트 */
-export const CardUnreviewed: Story = {
-  render: () => <UnreviewedCardItem {...mockData} />,
-};
-/** 마이페이지 내가 만든 모임 카드 컴포넌트 */
-export const CardCreatedGathering: Story = {
-  render: () => <CreatedCardItem {...mockData} />,
 };
