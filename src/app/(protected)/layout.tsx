07302ae5f5a2032544f35db21bd5ce91f0b4e721
hey-anna-wp <heyannawp@gmail.com>
2025-10-19 18:20:05 +0900

    chore(auth): 불필요한 AuthGuard 컴포넌트 및 관련 로직 제거
    - (protected) 레이아웃 삭제
    - AuthGuard 컴포넌트 삭제

--- src/app/(protected)/layout.tsx
+++
@@ -1,6 +0,0 @@
-// src/app/(protected)/layout.tsx
-import AuthGuard from "@/guards/AuthGuard";
-
-export default function ProtectedLayout({ children }: { children: React.ReactNode }) {
-  return <AuthGuard>{children}</AuthGuard>;
-}
