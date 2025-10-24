a569c68108a9969ca8b0997add30a60f42eddc92
Farmer-Kim <apple@apples-MacBook-Air.local>
2025-10-24 18:24:21 +0900

    fix: #90 Mock 데이터 타입 구조 수정
    - Review 타입에 맞게 mockAllReviews 데이터 구조 변경
    - User, Gathering 객체 중첩 구조로 수정
    - 타입 에러 해결 및 일관성 확보

--- src/mocks/reviews/mockAllReviews.ts
+++ src/mocks/reviews/mockAllReviews.ts
@@ -1,62 +1,97 @@
 export const mockAllReviews = [
   {
+    teamId: "11-6",
     id: 1,
-    userName: "럽윈즈올",
-    userImage: "/image/avatars/female.svg",
     score: 5,
+    comment: "강사분도 친절하시고 ~ ^^ 너무 좋은 공간에서 긴장과 스트레스 모두 잘 풀고 가요 ~ ^^",
     createdAt: "2024-01-25T00:00:00.000Z",
-    gatheringType: "오피스 스트레칭 이용",
-    gatheringLocation: "을지로 3가",
-    gatheringImage:
-      "https://sprint-fe-project.s3.ap-northeast-2.amazonaws.com/together-dallaem/1760431482840_68747470733a2f2f7365637572652e6d65657475707374617469632e636f6d2f70686f746f732f6576656e742f322f622f392f632f686967687265735f3531383239313136342e6a706567.avif",
-    comment:
-      "강사분도 친절하시고 ~ ^^ 너무 좋은 공간에서 긴장과 스트레스 모두 잘 풀고 가요 ~ ^^",
-    category: "DALLAEMFIT",
-    subCategory: "OFFICE_STRETCHING",
+    Gathering: {
+      teamId: "11-6",
+      id: 1,
+      type: "OFFICE_STRETCHING" as const,
+      name: "오피스 스트레칭 이용",
+      dateTime: "2024-01-25T00:00:00.000Z",
+      location: "을지로 3가",
+      image:
+        "https://sprint-fe-project.s3.ap-northeast-2.amazonaws.com/together-dallaem/1760431482840_68747470733a2f2f7365637572652e6d65657475707374617469632e636f6d2f70686f746f732f6576656e742f322f622f392f632f686967687265735f3531383239313136342e6a706567.avif",
+    },
+    User: {
+      teamId: "11-6",
+      id: 1,
+      name: "럽윈즈올",
+      image: "/image/avatars/female.svg",
+    },
   },
   {
+    teamId: "11-6",
     id: 2,
-    userName: "럽윈즈올",
-    userImage: "/image/avatars/female.svg",
     score: 5,
-    createdAt: "2024-01-25T00:00:00.000Z",
-    gatheringType: "마인드 풀니스 이용",
-    gatheringLocation: "을지로 3가",
-    gatheringImage:
-      "https://sprint-fe-project.s3.ap-northeast-2.amazonaws.com/together-dallaem/1760431482840_68747470733a2f2f7365637572652e6d65657475707374617469632e636f6d2f70686f746f732f6576656e742f322f622f392f632f686967687265735f3531383239313136342e6a706567.avif",
     comment: "차분하고 포근한 느낌 덕분에 처음인데도 금방 편해졌어요 !",
-    category: "DALLAEMFIT",
-    subCategory: "MINDFULNESS",
+    createdAt: "2024-01-25T00:00:00.000Z",
+    Gathering: {
+      teamId: "11-6",
+      id: 2,
+      type: "MINDFULNESS" as const,
+      name: "마인드 풀니스 이용",
+      dateTime: "2024-01-25T00:00:00.000Z",
+      location: "을지로 3가",
+      image:
+        "https://sprint-fe-project.s3.ap-northeast-2.amazonaws.com/together-dallaem/1760431482840_68747470733a2f2f7365637572652e6d65657475707374617469632e636f6d2f70686f746f732f6576656e742f322f622f392f632f686967687265735f3531383239313136342e6a706567.avif",
+    },
+    User: {
+      teamId: "11-6",
+      id: 2,
+      name: "럽윈즈올",
+      image: "/image/avatars/female.svg",
+    },
   },
   {
+    teamId: "11-6",
     id: 3,
-    userName: "당근조아",
-    userImage: "/image/avatars/male.svg",
     score: 4,
-    createdAt: "2024-01-25T00:00:00.000Z",
-    gatheringType: "오피스 스트레칭 이용",
-    gatheringLocation: "을지로 3가",
-    gatheringImage:
-      "https://sprint-fe-project.s3.ap-northeast-2.amazonaws.com/together-dallaem/1760431482840_68747470733a2f2f7365637572652e6d65657475707374617469632e636f6d2f70686f746f732f6576656e742f322f622f392f632f686967687265735f3531383239313136342e6a706567.avif",
     comment:
       "따듯하게 느껴지는 공간이에요 :) 평소에 달램 이용해보고 싶었는데 같이달램 생기니까 너무 좋아요! 프로그램이 더 늘어났으면 좋겠어요.",
-    category: "DALLAEMFIT",
-    subCategory: "OFFICE_STRETCHING",
+    createdAt: "2024-01-25T00:00:00.000Z",
+    Gathering: {
+      teamId: "11-6",
+      id: 3,
+      type: "OFFICE_STRETCHING" as const,
+      name: "오피스 스트레칭 이용",
+      dateTime: "2024-01-25T00:00:00.000Z",
+      location: "을지로 3가",
+      image:
+        "https://sprint-fe-project.s3.ap-northeast-2.amazonaws.com/together-dallaem/1760431482840_68747470733a2f2f7365637572652e6d65657475707374617469632e636f6d2f70686f746f732f6576656e742f322f622f392f632f686967687265735f3531383239313136342e6a706567.avif",
+    },
+    User: {
+      teamId: "11-6",
+      id: 3,
+      name: "당근조아",
+      image: "/image/avatars/male.svg",
+    },
   },
   {
+    teamId: "11-6",
     id: 4,
-    userName: "당근조아",
-    userImage: "/image/avatars/male.svg",
     score: 4,
-    createdAt: "2024-01-25T00:00:00.000Z",
-    gatheringType: "오피스 스트레칭 이용",
-    gatheringLocation: "을지로 3가",
-    gatheringImage:
-      "https://sprint-fe-project.s3.ap-northeast-2.amazonaws.com/together-dallaem/1760431482840_68747470733a2f2f7365637572652e6d65657475707374617469632e636f6d2f70686f746f732f6576656e742f322f622f392f632f686967687265735f3531383239313136342e6a706567.avif",
     comment:
-      "따���하게 느껴지는 공간이에요 :) 평소에 달램 이용해보고 싶었는데 같이달램 생기니까 너무 좋아요! 프로그램이 더 늘어났으면 좋겠어요.",
-    category: "DALLAEMFIT",
-    subCategory: "OFFICE_STRETCHING",
+      "따듯하게 느껴지는 공간이에요 :) 평소에 달램 이용해보고 싶었는데 같이달램 생기니까 너무 좋아요! 프로그램이 더 늘어났으면 좋겠어요.",
+    createdAt: "2024-01-25T00:00:00.000Z",
+    Gathering: {
+      teamId: "11-6",
+      id: 4,
+      type: "OFFICE_STRETCHING" as const,
+      name: "오피스 스트레칭 이용",
+      dateTime: "2024-01-25T00:00:00.000Z",
+      location: "을지로 3가",
+      image:
+        "https://sprint-fe-project.s3.ap-northeast-2.amazonaws.com/together-dallaem/1760431482840_68747470733a2f2f7365637572652e6d65657475707374617469632e636f6d2f70686f746f732f6576656e742f322f622f392f632f686967687265735f3531383239313136342e6a706567.avif",
+    },
+    User: {
+      teamId: "11-6",
+      id: 4,
+      name: "당근조아",
+      image: "/image/avatars/male.svg",
+    },
   },
 ];
 
