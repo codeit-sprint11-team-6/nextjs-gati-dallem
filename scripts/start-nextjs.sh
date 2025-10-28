adaf7b2dbcc7cb9411c71d9bffeb33eb6bcb3c9d
Farmer-Kim <apple@apples-MacBook-Air.local>
2025-10-28 09:38:29 +0900

    chore: start-nextjs.sh 스크립트 개선 및 테스트 이슈 템플릿 추가 (#38)
    - start-nextjs.sh 스크립트 개선
    - 테스트 구현 이슈 템플릿 추가 (.github/ISSUE_TEMPLATE/4-test-implementation.md)

--- scripts/start-nextjs.sh
+++ scripts/start-nextjs.sh
@@ -1,9 +1,11 @@
 #!/bin/bash 
+set -e  
 
 echo "------------ 어플리케이션 배포 시작 ------------"
-cd /home/ubuntu/nextjs-gati-dallem
+cd /home/ubuntu/nextjs-gati-dallem || exit 1
 npm ci
 pm2 kill
+sleep 2
 pm2 start npm --name "nextjs-app" -- start
 
 echo "------------ 어플리케이션 배포 완료 ------------"
