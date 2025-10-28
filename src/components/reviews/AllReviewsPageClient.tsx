69b9b8d646689346d72c1e625b2793f3acd46608
Farmer-Kim <apple@apples-MacBook-Air.local>
2025-10-28 11:05:56 +0900

    feat: 공통 컴포넌트 분리 및 재사용성 향상
    - PageHeader 컴포넌트 생성 (헤더 섹션 공통화)
    - FilterBar를 common 폴더로 이동 (여러 페이지에서 재사용)
    - AllReviewsPageClient에서 공통 컴포넌트 사용
    - 찜하기 페이지 등에서 재사용 가능하도록 구조 개선

--- src/components/reviews/AllReviewsPageClient.tsx
+++ src/components/reviews/AllReviewsPageClient.tsx
@@ -1,8 +1,8 @@
 "use client";
 
 import { useState } from "react";
-import Image from "next/image";
-import FilterBar, { MeetingFilters } from "@/components/meeting/list/FilterBar";
+import PageHeader from "@/components/common/PageHeader";
+import FilterBar, { MeetingFilters } from "@/components/common/FilterBar";
 import ReviewsRatingSummary from "@/components/reviews/ReviewsRatingSummary";
 import ReviewList from "@/components/reviews/ReviewList";
 import { useReviews, useReviewScores } from "@/apis/reviews/reviews.query";
@@ -138,19 +138,12 @@ export default function AllReviewsPageClient() {
   return (
     <div className="mx-auto max-w-7xl">
       {/* Header Section */}
-      <div className="mb-6 flex items-end gap-6 md:gap-7 lg:mb-8 lg:gap-[33px]">
-        <div className="relative h-14 w-[70px] flex-shrink-0 md:h-[57px] md:w-[70px] lg:h-[91px] lg:w-[97px]">
-          <Image src="/image/empty.svg" alt="리뷰 헤더 일러스트" fill className="object-contain" />
-        </div>
-        <div className="flex flex-col gap-2 md:gap-4">
-          <h1 className="text-lg leading-7 font-semibold text-gray-900 md:text-2xl lg:text-[32px] lg:leading-9">
-            모든 리뷰
-          </h1>
-          <p className="text-base leading-6 font-medium text-slate-400 md:text-lg md:leading-7 lg:text-xl lg:leading-7">
-            같이달램을 이용자들은 이렇게 느꼈어요 🫶
-          </p>
-        </div>
-      </div>
+      <PageHeader
+        imageSrc="/image/empty.svg"
+        imageAlt="리뷰 헤더 일러스트"
+        title="모든 리뷰"
+        description="같이달램을 이용자들은 이렇게 느꼈어요 🫶"
+      />
 
       {/* Filter Bar */}
       <FilterBar value={filters} onChange={setFilters} />
