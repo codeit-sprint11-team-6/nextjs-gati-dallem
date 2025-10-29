56f4f0ad678b01363a19e0f08721a628be3353ac
Farmer-Kim <apple@apples-MacBook-Air.local>
2025-10-29 16:30:42 +0900

    fix: AWS S3 이미지 로딩을 위한 HTTP/HTTPS 프로토콜 모두 허용
    - AWS S3: HTTP와 HTTPS 모두 지원
    - Unsplash: HTTPS만 지원 (보안상 적절)
    - AWS와 Vercel 환경 모두에서 이미지 로딩 가능

--- next.config.ts
+++ next.config.ts
@@ -6,11 +6,18 @@ const nextConfig: NextConfig = {
   },
   images: {
     remotePatterns: [
+      // AWS S3 - HTTP와 HTTPS 모두 허용
+      {
+        protocol: "http",
+        hostname: "sprint-fe-project.s3.ap-northeast-2.amazonaws.com",
+        pathname: "/together-dallaem/**",
+      },
       {
         protocol: "https",
         hostname: "sprint-fe-project.s3.ap-northeast-2.amazonaws.com",
         pathname: "/together-dallaem/**",
       },
+      // Unsplash - HTTPS만 허용
       {
         protocol: "https",
         hostname: "images.unsplash.com",
