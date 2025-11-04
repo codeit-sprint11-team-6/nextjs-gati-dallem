bc1dac9b0255d26772535b0618a6219394b7a700
Yoos99 <yoosh2520@naver.com>
2025-11-04 09:06:46 +0900

    fix: 스크롤 복원 메커니즘의 충돌 위험

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   experimental: {
     reactCompiler: true,
-    scrollRestoration: true,
   },
   images: {
     remotePatterns: [
